adea2f547d0ef998085f68e0ed5cb3b28689d1a2
Debumax <deboramarcial1991@gmail.com>
2021-04-12 22:31:57 -0300

    2do commit del archivo .cpp

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -3,6 +3,30 @@
 
 int main (){
 
-    printf("hola mamu");
+    int var=11;
+    int *pvar=&var;
+   // printf("contenido de la variable: %d\n",var);
+   
+    //2-a
+    printf("contenido de lo que apunta el puntero: %d\n",*pvar);
+    //2-b
+    printf("contenido del puntero: %u\n",pvar);
+    //2-c
+    printf("direccion de memoria de la variable: %u\n",&var);
+    //2-d
+    printf("direccion de memoria de donde esta ubicado el puntero: %u\n",&pvar);
+    //2-e
+    printf("tamanio del puntero: %d",1*sizeof(var));
+
+    /*printf("con diferentes notaciones \n");
+    printf("contenido del puntero con notacion de hexadecimal sin signo: %x\n",pvar);
+    printf("contenido del puntero con notacion de decimal sin signo: %u\n",pvar);
+    printf("contenido del puntero con notacion de puntero: %p\n",pvar);*/
+
+
+
+    getchar();
+
+    
     return 0;
 }
